234ac4b1e6622c69f2971df2bcc2422c68fbdb94
CabeLovlie <cabeklovlie@gmail.com>
2022-09-19 09:51:43 -0700

    Creates a console rock, paper, scissors game
    This is a console based rock, paper, scissors game.  It prompts
    the user for a selection, compares that to a random selection
    made by the computer, and returns the winner result. It plays over
    5 rounds and keeps track of a score.  At the end it announces a
    winner.

---
+++ index.js
@@ -0,0 +1,67 @@
+let computerScore = 0;
+let playerScore = 0;
+
+//The program promps the user to select Rock, Paper, or Scissors
+function getPlayerChoice() {
+    string = prompt("Choose Rock, Paper, or Scissors!");
+    let playerSelection = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
+    return playerSelection;
+}
+
+//The program then randomly generate the computer's selection
+function getComputerChoice() {
+    const rockPaperScissorsArray = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * rockPaperScissorsArray.length);
+
+    return rockPaperScissorsArray[random];
+}
+
+//The program compares the selection of the user to that of the computer
+//based on: Rock > Scissors, Scissors > Paper, and Paper > Rock
+function playRound(playerChoice, computerChoice) {
+    let roundResult;
+
+    if (playerChoice === computerChoice) {
+        roundResult = `Tie game! You both picked ${playerChoice}`
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        roundResult = "Rock beats Scissors! You win!"
+        playerScore++;
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        roundResult = "Scissors beats Paper! You win!"
+        playerScore++;
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        roundResult = "Paper beats Rock! You win!"
+        playerScore++;
+    } else {
+        roundResult = `${computerChoice} beats ${playerChoice}! You lose!`
+        computerScore++;
+    }
+
+    alert(roundResult);
+    console.log(roundResult);
+    return computerScore, playerScore;
+}
+
+//Play 5 rounds, keeping track of the winner for each round
+function game() {
+    for (let i = 0; i <= 4; i++){
+        let computerChoice = getComputerChoice();
+        let playerChoice = getPlayerChoice();
+        playRound(playerChoice, computerChoice);
+    }
+
+    finalScore();
+}
+
+
+//At the end of 5 rounds display the finial winner and score
+function finalScore(playerScore, computerScore){
+    if (playerScore > computerScore) {
+        alert(`You win the game ${playerScore} rounds to ${computerScore}!`)
+    } else if (computerScore > playerScore) {
+        alert(`You lose the game ${computerScore} rounds to ${playerScore}!`)
+    } else {
+        alert("The game ended in a tie!")
+    }
+}
